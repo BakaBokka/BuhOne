f0b33dd4b23c3f19bcd70f78e4a6a5f3a06df782
Damir Vakhitov <damirvakhitov@gmail.com>
2020-11-24 23:56:20 +0300

    fix: Правки формы

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import { useFormik } from "formik";
+import classNames from "classnames/bind";
 import "./ContactForm.scss";
 
 //Стили для кнопки
@@ -10,33 +11,45 @@ const buttonStyle = {
   right: "0",
 };
 
-//Валидация
-const validate = (values) => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = "Это обязательное поле";
-  } else if (values.firstName.length > 20) {
-    errors.firstName = "Ой, больше 20 символов нельзя";
-  }
-
-  if (!values.lastName) {
-    errors.lastName = "Это обязательное поле";
-  } else if (values.lastName.length > 20) {
-    errors.lastName = "Ой, больше 20 символов нельзя";
-  }
-
-  if (!values.userMessage) {
-    errors.userMessage = "Это обязательное поле";
-  } else if (!/^[а-яА-ЯёЁa-zA-Z0-9\s]+$/i.test(values.userMessage)) {
-    errors.userMessage = "Только буквы или цифры, пожалуйста";
-  }else if (values.userMessage.length > 100) {
-    errors.userMessage = "Ой, больше 100 символов нельзя";
-  }
-
-  return errors;
-};
-
 const ContactForm = () => {
+  const [isInvalid, setInvalid] = useState(false);
+
+  //Валидация
+  const validate = (values) => {
+    const errors = {};
+    if (!values.firstName) {
+      errors.firstName = "Это обязательное поле";
+      setInvalid(true);
+    } else if (values.firstName.length > 20) {
+      errors.firstName = "Ой, больше 20 символов нельзя";
+      setInvalid(true);
+    }
+
+    if (!values.lastName) {
+      errors.lastName = "Это обязательное поле";
+      setInvalid(true);
+    } else if (values.lastName.length > 20) {
+      errors.lastName = "Ой, больше 20 символов нельзя";
+      setInvalid(true);
+    }
+
+    if (!values.userMessage) {
+      errors.userMessage = "Это обязательное поле";
+      setInvalid(true);
+    } else if (!/^[а-яА-ЯёЁa-zA-Z0-9\s]+$/i.test(values.userMessage)) {
+      errors.userMessage = "Только буквы или цифры, пожалуйста";
+      setInvalid(true);
+    } else if (values.userMessage.length > 100) {
+      errors.userMessage = "Ой, больше 100 символов нельзя";
+      setInvalid(true);
+    }
+
+    return errors;
+  };
+
+  //Класс подсвечивает поле красным, если валидация провалилась
+  const validClass = [{ ContactForm__input_invalid: isInvalid === true }];
+
   const formik = useFormik({
     initialValues: {
       firstName: "",
@@ -49,7 +62,11 @@ const ContactForm = () => {
     },
   });
   return (
-    <form className="ContactForm__form" onSubmit={formik.handleSubmit}>
+    <form
+      className="ContactForm__form"
+      onSubmit={formik.handleSubmit}
+      autoComplete="off"
+    >
       <div className="ContactForm__content">
         <div className="ContactForm__container">
           <div className="ContactForm__inputs">
@@ -57,17 +74,23 @@ const ContactForm = () => {
               Имя
             </label>
             <input
-              className="ContactForm__input"
+              className={classNames(
+                "ContactForm__input",
+                formik.touched.firstName && formik.errors.firstName
+                  ? validClass
+                  : ""
+              )}
               id="firstName"
               name="firstName"
               type="text"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.firstName}
               maxLength="21"
               placeholder="Иван"
             />
             <div className="ContactForm__error">
-              {formik.errors.firstName ? (
+              {formik.touched.firstName && formik.errors.firstName ? (
                 <div>{formik.errors.firstName}</div>
               ) : null}
             </div>
@@ -77,17 +100,23 @@ const ContactForm = () => {
               Фамилия
             </label>
             <input
-              className="ContactForm__input"
+              className={classNames(
+                "ContactForm__input",
+                formik.touched.lastName && formik.errors.lastName
+                  ? validClass
+                  : ""
+              )}
               id="lastName"
               name="lastName"
               type="text"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               value={formik.values.lastName}
               maxLength="21"
               placeholder="Иванов"
             />
             <div className="ContactForm__error">
-              {formik.errors.lastName ? (
+              {formik.touched.lastName && formik.errors.lastName ? (
                 <div>{formik.errors.lastName}</div>
               ) : null}
             </div>
@@ -98,22 +127,32 @@ const ContactForm = () => {
           Сообщение
         </label>
         <textarea
-          className="ContactForm__textarea"
+          className={classNames(
+            "ContactForm__textarea",
+            formik.touched.userMessage && formik.errors.userMessage
+              ? validClass
+              : ""
+          )}
           id="userMessage"
           name="userMessage"
           type="text"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.userMessage}
           maxLength="101"
           placeholder="Ваше сообщение"
         />
         <div className="ContactForm__error ContactForm__error_textarea">
-          {formik.errors.userMessage ? (
+          {formik.touched.userMessage && formik.errors.userMessage ? (
             <div>{formik.errors.userMessage}</div>
           ) : null}
         </div>
       </div>
-      <Button className="ContactForm__button" text={"Отправить сообщение"} style={buttonStyle}/>
+      <Button
+        className="ContactForm__button"
+        text={"Отправить сообщение"}
+        style={buttonStyle}
+      />
     </form>
   );
 };
